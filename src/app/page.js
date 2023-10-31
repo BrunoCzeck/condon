07f8297215fc82f05b0b5ea2ef83aafe4481f0f6
BrunoCzeck <brunoczeck@hotmail.com>
2023-10-31 18:40:32 -0300

    Ajuste na rota do post

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ function App() {
     const data = { usuario, senha, apartament, bloc, email, id_enterprise };
 /* # LINK PARA TESTAR EM PRODUÇÃO */
 /*     axios.post('https://api-condon-production.up.railway.app/api/v1/users', data, { */     
- axios.post('http://127.0.0.1:3333/api/v1/users', data, {
+ axios.post('https://api-condon-production.up.railway.app/api/v1/users', data, {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
