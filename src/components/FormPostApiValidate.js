45bc5b9409afaa5630ecfdbc048316edb924cf6f
BrunoCzeck <brunoczeck@hotmail.com>
2023-11-19 05:10:59 -0300

    Add reposit package

--- src/components/FormPostApiValidate.js
+++ src/components/FormPostApiValidate.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const getData = (data) => {
 /*  PRODUÇÃO */
 /*   return axios.get('https://api-condon-production.up.railway.app/api/v1/users/usersvalidate', data, {  */  
-  return axios.post('http://127.0.0.1:3333/api/v1/users/usersvalidate', data, {
+  return axios.post('https://api-condon-production.up.railway.app/api/v1/users/usersvalidate', data, {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
